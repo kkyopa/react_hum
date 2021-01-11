db02fb4dc97f50596f68eaeb2a20ec834a13a8c0
川田京助 <kkyopa@gmail.com>
2021-01-11 21:14:20 +0900

    型定義

--- src/CounterReducer.tsx
+++ src/CounterReducer.tsx
@@ -1,13 +1,17 @@
 import React, { useReducer, useEffect, useRef, useState } from "react";
 
+type StateType = { count: number };
 const initialState = { count: 0 };
+type ActionType = { type: "decrement" | "increment" | "reset" };
 
-function reducer(state: any, action: any) {
+function reducer(state: StateType, action: ActionType): StateType | never {
   switch (action.type) {
     case "increment":
       return { count: state.count + 1 };
     case "decrement":
       return { count: state.count - 1 };
+    case "reset":
+      return initialState;
     default:
       throw new Error();
   }
@@ -20,6 +24,7 @@ function CounterReducer() {
       Count: {state.count}
       <button onClick={() => dispatch({ type: "decrement" })}>-</button>
       <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </>
   );
 }
