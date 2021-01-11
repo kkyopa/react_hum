8c45666d82fd9812b1dee02d4a001fb99ad5e722
川田京助 <kkyopa@gmail.com>
2021-01-11 19:55:52 +0900

    propsテスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+interface AppProps {
+  message?: string;
+  name: string;
+}
+
+const App: React.FC<AppProps> = ({ message, name }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {message}
+      {name}
     </div>
   );
-}
+};
+
+App.defaultProps = {
+  message: "設定してない",
+};
 
 export default App;
